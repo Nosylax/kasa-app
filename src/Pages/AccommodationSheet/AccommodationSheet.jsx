691ca337aa9008bd83945933012519824b48be25
Nosylax <alyson.laville@gmail.com>
2024-09-13 19:18:42 +0200

    Redirection page 404 mauvaise id

--- src/Pages/AccommodationSheet/AccommodationSheet.jsx
+++ src/Pages/AccommodationSheet/AccommodationSheet.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Redirect, useNavigate } from "react-router-dom";
 import "./AccommodationSheet.css";
 import Collapse from "../../components/Collapse/Collapse";
 import Slideshow from "../../components/Slideshow/Slideshow";
@@ -8,73 +9,84 @@ import { useParams } from "react-router-dom";
 export default function AccommodationSheet() {
   const param = useParams();
   console.log(param);
-  const [location, setLocation] = useState([]);
+  const [location, setLocation] = useState(null);
 
+  // const history = useHistory();
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(
           "http://localhost:8080/api/properties/" + param.id
         );
-        const data = await response.json();
-        setLocation(data);
+        if (response.status === 200) {
+          const data = await response.json();
+          console.log(data);
+          setLocation(data);
+        } else {
+          navigate("/NotFound");
+        }
+        // if (data == null) {
+        //   history.push("/NotFound");
+        // }
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchData();
-  }, [param]);
+  }, [param, navigate]);
   console.log(location);
 
+  if (!location) {
+    return <p>chargement en cours</p>;
+  }
   return (
     <>
-      {location.title !== undefined ? (
-        <div className="accommodationSheetContainer">
-          <Slideshow pictures={location.pictures} />
-          <div className="accommodationSheetInfos">
-            <div className="titleNamePicture">
-              <div className="locationTitle">
-                {location.title}
-                <p className="locationLocation">{location.location}</p>
-              </div>
-              <div className="HostAndPicture">
-                <div className="locationHostName">{location.host.name}</div>
-                <div className="hostPictureBox">
-                  <img
-                    className="hostPicture"
-                    src={location.host.picture}
-                    alt={`Portrait du propriétaire`}
-                  />
-                </div>
-              </div>
+      <div className="accommodationSheetContainer">
+        <Slideshow pictures={location.pictures} />
+        <div className="accommodationSheetInfos">
+          <div className="titleNamePicture">
+            <div className="locationTitle">
+              {location.title}
+              <p className="locationLocation">{location.location}</p>
             </div>
-            <div className="locationTags">
-              {location.tags.map((tag, locationTag) => (
-                <p className="locationTag" key={locationTag}>
-                  {tag}
-                </p>
-              ))}
-              {/* <StartRating /> */}
-            </div>
-            <div className="locationCollapses">
-              <Collapse title="Description" subtitle={location.description} />
-              <Collapse
-                title="Equipements"
-                subtitle={location.equipments.map(
-                  (equipment, locationEquipment) => (
-                    <p className="locationEquipments" key={locationEquipment}>
-                      {equipment}
-                    </p>
-                  )
-                )}
-              />
+            <div className="HostAndPicture">
+              <div className="locationHostName">{location.host.name}</div>
+              <div className="hostPictureBox">
+                <img
+                  className="hostPicture"
+                  src={location.host.picture}
+                  alt={`Portrait du propriétaire`}
+                />
+              </div>
             </div>
           </div>
         </div>
-      ) : (
-        <div />
-      )}
+        <div className="locationTags">
+          {location.tags.map((tag, locationTag) => (
+            <p className="locationTag" key={locationTag}>
+              {tag}
+            </p>
+          ))}
+          {/* <StartRating /> */}
+        </div>
+        <div>
+          <div className="locationCollapses">
+            <Collapse title="Description" subtitle={location.description} />
+            <Collapse
+              title="Equipements"
+              subtitle={location.equipments.map(
+                (equipment, locationEquipment) => (
+                  <p className="locationEquipments" key={locationEquipment}>
+                    {equipment}
+                  </p>
+                )
+              )}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
